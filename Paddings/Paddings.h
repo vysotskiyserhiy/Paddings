c0d8db7ea7b0116c26d86efa92eb3d1d98245d50
Serge Vysotsky <vysotskiyserhiy@gmail.com>
2019-06-26 23:40:09 +0300

    Initial Commit

---
+++ Paddings/Paddings.h
@@ -0,0 +1,19 @@
+//
+//  Paddings.h
+//  Paddings
+//
+//  Created by Serhiy Vysotskiy on 26.06.2019.
+//  Copyright © 2019 Serhiy Vysotskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Paddings.
+FOUNDATION_EXPORT double PaddingsVersionNumber;
+
+//! Project version string for Paddings.
+FOUNDATION_EXPORT const unsigned char PaddingsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Paddings/PublicHeader.h>
+
+
